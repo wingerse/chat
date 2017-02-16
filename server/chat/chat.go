9e6f51f49f8c0d04146f8244e86634836b16b8d4
wsendon <aiman012@live.com>
2017-02-17 00:08:23 +0500

    Prevent duplicate named clients

--- server/chat/chat.go
+++ server/chat/chat.go
@@ -5,11 +5,17 @@ import "bufio"
 import "strconv"
 import "sync"
 import "strings"
-import "fmt"
+import "log"
+import "os"
 
 type message struct {
 	message string
 	client  *client
+}
+
+type nameCheck struct {
+	name    string
+	present bool
 }
 
 func (m message) getFormatted() string {
@@ -22,14 +28,15 @@ type client struct {
 }
 
 type Server struct {
-	l           net.Listener
-	clients     map[*client]struct{}
-	messageChan chan message
-	addedChan   chan *client
-	removedChan chan *client
-	port        uint16
-	name        string
-	m           sync.RWMutex
+	l             net.Listener
+	clients       map[*client]struct{}
+	messageChan   chan message
+	addedChan     chan *client
+	removedChan   chan *client
+	nameCheckChan chan nameCheck
+	port          uint16
+	name          string
+	m             sync.RWMutex
 }
 
 // NewServer starts listening, and returns an initialized server. If an error occured while listening started, (nil, error) is returned.
@@ -40,17 +47,20 @@ func NewServer(name string, port uint16) (*Server, error) {
 		return nil, e
 	}
 	return &Server{l: l,
-			clients:     make(map[*client]struct{}),
-			messageChan: make(chan message, 2),
-			addedChan:   make(chan *client, 2),
-			removedChan: make(chan *client, 2),
-			port:        port,
-			name:        name},
+			clients:       make(map[*client]struct{}),
+			messageChan:   make(chan message, 2),
+			addedChan:     make(chan *client, 2),
+			removedChan:   make(chan *client, 2),
+			nameCheckChan: make(chan nameCheck),
+			port:          port,
+			name:          name},
 		nil
 }
 
 // Start starts accepting clients + managing messages.
 func (r *Server) Start() {
+	log.SetOutput(os.Stdout)
+	log.Println("starting server at "+r.l.Addr().String())
 	go r.handleMessages()
 	for {
 		conn, e := r.l.Accept()
@@ -69,6 +79,12 @@ func (r *Server) handleClient(conn net.Conn) {
 	}
 	name := strings.TrimSpace(scanner.Text())
 
+	r.nameCheckChan <- nameCheck{name: name}
+	if n := <-r.nameCheckChan; n.present {
+		conn.Write([]byte("A user with that name is already online. Choose another name\n"))
+		return
+	}
+
 	c := &client{conn, name}
 	r.addClient(c)
 	defer r.removeClient(c)
@@ -82,7 +98,7 @@ func (r *Server) handleClient(conn net.Conn) {
 				if t.Timeout() {
 					return
 				}
-			} 
+			}
 			continue
 		}
 		r.messageChan <- message{scanner.Text(), c}
@@ -91,22 +107,31 @@ func (r *Server) handleClient(conn net.Conn) {
 
 func (r *Server) handleMessages() {
 	for {
+		outerselect:
 		select {
+		case n := <-r.nameCheckChan:
+			for k := range r.clients {
+				if k.name == n.name {
+					r.nameCheckChan <- nameCheck{n.name, true}
+					break outerselect
+				}
+			}
+			r.nameCheckChan <- nameCheck{n.name, false}
 		case c := <-r.addedChan:
 			r.clients[c] = struct{}{}
 			r.publishMessage(c.name + " has connected to the server\n")
-			fmt.Println(c.name + "(" + c.conn.RemoteAddr().String() + ")" + " has disconnected from the server")
+			log.Println(c.name + "(" + c.conn.RemoteAddr().String() + ")" + " has connected to the server")
 		case m := <-r.messageChan:
 			for k := range r.clients {
 				if k != m.client {
 					go k.conn.Write([]byte(m.getFormatted()))
 				}
 			}
-			fmt.Print(m.getFormatted())
+			log.Print(m.getFormatted())
 		case c := <-r.removedChan:
 			delete(r.clients, c)
 			r.publishMessage(c.name + " has disconnected from the server\n")
-			fmt.Println(c.name + "(" + c.conn.RemoteAddr().String() + ")" + " has disconnected from the server")
+			log.Println(c.name + "(" + c.conn.RemoteAddr().String() + ")" + " has disconnected from the server")
 		}
 	}
 }
